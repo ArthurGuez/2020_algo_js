226ea99107a8cc99adedbcfa20837dc863bc46f1
Arthur Guez <Arthur@MacBook-Air-de-Arthur.local>
2020-03-22 18:51:21 +0100

    Exo 1 termné

---
+++ ex01/ex01.js
@@ -0,0 +1,16 @@
+const num = 1;
+const string = "js";
+const array = ['apple', 'banana'];
+const bool = true;
+const person = {
+    firstName: "Arthur",
+    lastName: 'Dunno',
+    age: 25,
+    eyeColor: "Brown"
+};
+
+console.log(typeof num);
+console.log(typeof string);
+console.log(typeof array);
+console.log(typeof bool);
+console.log(typeof person);
